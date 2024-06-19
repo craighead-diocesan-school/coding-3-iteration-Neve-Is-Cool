42ea0d32810fa4faad08231b8d0dcefb32fa3ca3
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-19 16:31:37 +1200

    lift off

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -3,7 +3,7 @@
 // ####################################
 
 function liftOff() {
-  let count = 10
+  let count = prompt ('Pick a random number?')
  
   while (count > 0) {
     alert(count + '...')
@@ -15,5 +15,18 @@ function liftOff() {
 
 function make1() {
   // copy and paste your code here when you get to the Make step
+
+let max = prompt('Enter a maximum number...')
+let inc = prompt ('Enter an increment for that number to count down in...')
+let count = 0
+max =Number (max)
+inc =Number (inc)
+while (count <= max) {
+  alert (count)
+  left = max - count
+  count = count + inc
+}
+
+alert ('All done, with ' + left + ' leftover!!')
   
 }
