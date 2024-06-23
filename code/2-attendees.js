486c13568c6da35381b0f9f88c598a443a024032
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-24 10:07:02 +1200

    attendees + extension

--- code/2-attendees.js
+++ code/2-attendees.js
@@ -4,13 +4,12 @@
 
 function attendees() {
   let count = 0
-  let numOfAttendees = 5
+  let numOfAttendees = prompt  ('How many attendees will their be.')
   let attendeeList = ''
   
   while (count < numOfAttendees) {
-    let attendee = prompt()
-    attendeeList = attendeeList + attendee
-    attendeeList = attendeeList + ', '
+    let attendee = prompt('What are their names?')
+    attendeeList = attendeeList + attendee + ', '
     count = count + 1
   }
   
@@ -18,6 +17,25 @@ function attendees() {
 }
 
 function make2() {
-  // copy and paste your code here when you get to the Make step
+  
+  let number = prompt ('How many numbers do you want to add together?')
+
+  let totalNumOfNum = number
+
+  let count = 0
+  let oldNumber = 0
+  number = Number (number)
+
+  while (count < number) {
+    let newNumber = prompt ('Enter a number')
+    newNumber = Number (newNumber)
+    oldNumber = oldNumber + newNumber
+    oldNumber = Number (oldNumber)
+    count = count + 1
+  }
+
+  alert (oldNumber)
+  let average = oldNumber / totalNumOfNum
+  alert ('The average of your ' + totalNumOfNum + ' numbers is ' + average)
   
 }
