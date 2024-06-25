b75a13e6652e69e3d9c6d90b78532ed8d8a9242a
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-25 15:52:19 +1200

    factorial and square series

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -4,4 +4,18 @@
 
 function squareSeries() {
   // write your code here
+
+ 
+let number = prompt ('What number do you want the Square Series for?')
+number = Number (number)
+let series = ''
+
+while (number > 0) {
+square = number * number 
+series += square + ' ,'
+number = number -1
+}
+
+alert (series)
+
 }
--- code/4-test-factorial.js
+++ code/4-test-factorial.js
@@ -3,5 +3,15 @@
 // ####################################
 
 function factorial() {
-  // write your code here
+  
+  let num = prompt ('What number do you want the Factorial for')
+  num = Number (num)
+  let factorNum = 1
+
+  while (factorNum> 0) {
+    factorNum = num * num
+    num = num - 1
+  }
+  
+  alert ('The factorial is...       ' + factorNum)
 }
